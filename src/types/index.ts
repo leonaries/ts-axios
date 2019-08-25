17caac7ebbf0bbccc373208f99be0f7128aa9538
leon <315685499@qq.com>
2019-08-26 00:34:21 +0800

    interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,10 @@
-export type Method = 'get' |  'GET'
-| 'delete' | 'Delete'
-| 'head' | 'HEAD'
-| 'options' | 'OPTIONS'
-| 'post' | 'POST'
-| 'put' | 'PUT'
-| 'patch' | 'PATCH'
+export type Method = 'get' | 'GET'
+  | 'delete' | 'Delete'
+  | 'head' | 'HEAD'
+  | 'options' | 'OPTIONS'
+  | 'post' | 'POST'
+  | 'put' | 'PUT'
+  | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
   url:string,
